de021bdac148b75fe9d5ace95069f6767a443b03
Ivanka <IvNagornyuk@gmail.com>
2025-12-12 00:52:08 +0200

    changed task

--- src/hw-03.js
+++ src/hw-03.js
@@ -1,20 +1,22 @@
 // Завдання 1.
-const firstNum = +prompt('Введіть перше число');
-const secondNum = +prompt('Введіть ще одне число');
+const firstNum = Number(prompt("Введіть перше число"));
+const secondNum = Number(prompt("Введіть ще одне число"));
 const numberToCheck = 5;
 if ((firstNum + secondNum) % numberToCheck === 0) {
-    alert('Сума чисел ' + firstNum + ' i ' + secondNum + ' кратна ' + numberToCheck)
+  alert(
+    "Сума чисел " + firstNum + " i " + secondNum + " кратна " + numberToCheck
+  );
 } else {
-    alert('Сума не кратна ' + numberToCheck)
+  alert("Сума не кратна " + numberToCheck);
 }
 
 // Завдання 2.
-const itemMenu = +prompt(
-  "Виберіть напій з меню: \n 1 - чай \n 2 - кава \n 3 - сік \n 4 - вода"
+const itemMenu = Number(
+  prompt("Виберіть напій з меню: \n 1 - чай \n 2 - кава \n 3 - сік \n 4 - вода")
 );
 
 switch (itemMenu) {
-    case 1:
+  case 1:
     alert("Ви обрали чай");
     break;
   case 2:
@@ -31,16 +33,16 @@ switch (itemMenu) {
     alert("Такого напою у нас немає");
 }
 
-// // Завдання 3.
-const amount = +prompt("Введіть суму покупки");
+// Завдання 3.
+const amount = Number(prompt("Введіть суму покупки"));
 let discontAmount;
-const discont3 = amount * 0.03;
-const discont5 = amount * 0.05;
+const discont3 = 3;
+const discont5 = 5;
 if (amount > 800) {
-  discontAmount = amount - discont5;
+  discontAmount = amount - (amount / 100) * discont5;
   alert("Сума вашої покупки зі знижкою 5% - " + discontAmount);
 } else if (amount < 800 && amount >= 500) {
-  discontAmount = amount - discont3;
+  discontAmount = amount - (amount / 100) * discont3;
   alert("Сума вашої покупки зі знижкою 3% - " + discontAmount);
 } else if (amount > 0 && amount < 500) {
   discontAmount = amount;
@@ -50,6 +52,6 @@ if (amount > 800) {
 }
 
 // Завдання 4.
-const number1 = +prompt('Введіть перше число');
-const number2 = +prompt('Введіть ще одне число');
-console.log(number1 % number2 === 0)
+const number1 = Number(prompt("Введіть перше число"));
+const number2 = Number(prompt("Введіть ще одне число"));
+console.log(number1 % number2 === 0 || false);
